04aff4f59dab0d67392bbe86655ff062054a4e7c
CaretCoders <caretcoders@gmail.com>
2026-01-02 22:26:55 +0530

    Second Commit

---
+++ comparision.js
@@ -0,0 +1,12 @@
+// Comparison must be between two same  data type console.log("2" > 1); 
+// comparison always provide a boolean value
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+
+
+
+console.log();
---
+++ operations.js
@@ -0,0 +1,9 @@
+//****OPERATIONS*****//
+let boa = 5
+let negBoa = -boa
+console.log(negBoa);
+
+// To add stings via Operations
+let charmander = "firetype "
+let goCharmander = "pokemon"
+console.log(charmander + goCharmander);
