9031ee8c2ab9daa8228a7a4c11a89b267c8b72d9
CaretCoders <caretcoders@gmail.com>
2026-01-02 22:12:33 +0530

    First commit

---
+++ basic.js
@@ -0,0 +1,22 @@
+console.log("I am a test file.")
+// Console.log() - for output or print a file
+
+var Anaconda = "456"
+// To declare a variable use 
+// const= for unchangable variable
+// var= for changable variable 
+// let to declare a variable 
+
+console.log(typeof (Anaconda));
+// USe above to find type of Variable String, Number, Boolean, Nan
+// Remeber Null is an Object
+
+var Anaconda = 456
+console.log(Anaconda);
+console.log(typeof (Anaconda));
+
+let python = Boolean("Ramesh")
+console.log(python);
+//To declare Variable's Value with specifing data type use above  
+
+
