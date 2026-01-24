5c1df3bb6f0d7947ddd8972743508d8cb8364ae5
CaretCoders <caretcoders@gmail.com>
2026-01-24 12:42:00 +0530

    DATE OBJECT

--- 09_date&time.js
+++ 09_date&time.js
@@ -7,7 +7,7 @@ console.log(myBirthday);
 console.log(typeof myBirthday);
 
 // Month in Date object is 0 indexed.
-let herbirthday = new Date(2000, 05, 23); // Year, Month (0-11), Day
+let herbirthday = new Date(2000, 5, 23); // Year, Month (0-11), Day
 console.log(herbirthday);
 
 // To format date formats we can use different methods available with Date object.
@@ -15,3 +15,23 @@ console.log(new Date().toLocaleString(herbirthday));
 console.log(new Date().toDateString(herbirthday));
 console.log(new Date().toTimeString(herbirthday));
 console.log(new Date().toISOString(herbirthday));
+
+// Lets declarea custom date in date object where the .todatestring is used to format 
+let myEmidatebe = new Date(2026, 01, 20);
+console.log(myEmidatebe.toDateString());
+
+// Lets get the individual components of date and time using get methods.
+console.log(myEmidatebe.getMonth());
+console.log(myEmidatebe.getMonth() + 1); // Adding 1 to get the correct month number
+console.log(myEmidatebe.getDate());
+console.log(myEmidatebe.getFullYear());
+
+// To define an individeual property of date object we can use set methods.
+myEmidatebe.setFullYear(2025);
+console.log(myEmidatebe.toDateString());
+let chickenDinnerDay = new Date();
+chickenDinnerDay.toLocaleString('default', {
+    month: "long",
+    day: "2-digit",
+});
+console.log(chickenDinnerDay);
